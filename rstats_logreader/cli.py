506d4fe45b33d1eb0cc6e6df5dbb97dd7cda6059
Jeremy Brown <mischif@users.noreply.github.com>
2020-01-29 14:36:39 -0500

    [skip ci] Update file headers

--- rstats_logreader/cli.py
+++ rstats_logreader/cli.py
@@ -3,7 +3,7 @@
 ################################################################################
 #                               rstats-logreader                               #
 #   Parse RStats logfiles, display bandwidth usage, convert to other formats   #
-#                            (C) 2016, 2019 Mischif                            #
+#                       (C) 2016, 2019-2020 Jeremy Brown                       #
 #       Released under version 3.0 of the Non-Profit Open Source License       #
 ################################################################################
 
